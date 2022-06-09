144ee179b17e6f87f7841168fe35742a2c9cf634
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-06-09 21:57:58 +0300

    Work on RecipesApp

--- src/RecipesApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/RecipesApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -87,7 +87,7 @@ namespace RecipesApp.Areas.Identity.Pages.Account
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
+            [Display(Name = "Парола")]
             public string Password { get; set; }
 
             /// <summary>
@@ -95,8 +95,8 @@ namespace RecipesApp.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Display(Name = "Парола")]
+            [Compare("Password", ErrorMessage = "Паролата и паролата за потвърждаване не съвпадат")]
             public string ConfirmPassword { get; set; }
         }
 
