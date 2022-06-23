76b877827b2b3834aa5c6bbc076a7374ba158ee9
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-06-23 12:34:55 +0300

    Add IngredientsService GetAll method

---
+++ src/RecipesApp.Core/Services/IngredientsService.cs
@@ -0,0 +1,34 @@
+﻿using RecipesApp.Core.Contracts;
+using RecipesApp.Core.Models;
+using RecipesApp.Infrastructure.Data;
+using RecipesApp.Infrastructure.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipesApp.Core.Services
+{
+    public class IngredientsService : IIngredientsService
+    {
+        private readonly IApplicationDbRepository repo;
+
+        public IngredientsService(IApplicationDbRepository _repo)
+        {
+            repo = _repo;
+        }
+
+        public IEnumerable<IngredientNameIdViewModel> GetAll()
+        {
+            var ingredients = repo.All<Ingredient>()
+                .Select(x => new IngredientNameIdViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                }).ToList();
+
+            return ingredients;
+        }
+    }
+}
