aad58aa098f81bfa68ebde53f9b707e0e5e94f10
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-06-23 12:06:05 +0300

    Add SearchRecipesController

---
+++ src/RecipesApp/Controllers/SearchRecipesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecipesApp.Controllers
+{
+    public class SearchRecipesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
