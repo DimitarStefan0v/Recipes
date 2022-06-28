5fadda6366cb16b4db37064eacb1a3efce10433d
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-06-28 22:31:24 +0300

    Add IndexCategoryViewModel

---
+++ src/RecipesApp.Core/Models/IndexCategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RecipesApp.Core.Models
+{
+    public class IndexCategoryViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImgUrl { get; set; }
+    }
+}
