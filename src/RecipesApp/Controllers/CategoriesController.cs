524de8e20035d8e9fdb0f012c424506115a53947
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-06-29 12:20:29 +0300

    Add CategoriesController

---
+++ src/RecipesApp/Controllers/CategoriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecipesApp.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
