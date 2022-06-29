f4987f0c3986ae06f45c2d973fd11231281c0dff
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-06-29 18:35:55 +0300

    Add CommentsService

---
+++ src/RecipesApp.Core/Services/CommentsService.cs
@@ -0,0 +1,36 @@
+﻿using RecipesApp.Core.Contracts;
+using RecipesApp.Core.Models;
+using RecipesApp.Infrastructure.Data;
+using RecipesApp.Infrastructure.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipesApp.Core.Services
+{
+    public class CommentsService : ICommentsService
+    {
+        private readonly IApplicationDbRepository repo;
+
+        public CommentsService(IApplicationDbRepository _repo)
+        {
+            repo = _repo;
+        }
+
+        public async Task AddAsync(CommentInputModel input, string userId)
+        {
+            var comment = new Comment
+            {
+                Content = input.Content,
+                CreatedOn = DateTime.UtcNow,
+                AddedByUserId = userId,
+                RecipeId = input.RecipeId,
+            };
+
+            await repo.AddAsync(comment);
+            await repo.SaveChangesAsync();
+        }
+    }
+}
