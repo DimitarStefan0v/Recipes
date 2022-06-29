41eaeca3b01691c1a2e7538b6917ceacf1361dba
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-06-29 18:42:51 +0300

    Add error messages for comment input model

---
+++ src/RecipesApp.Core/Constants/CommentErrorMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace RecipesApp.Core.Constants
+{
+    public static class CommentErrorMessages
+    {
+        public const string CommentContentRequired = "Съдържание към коментара е задължително";
+
+        public const string CommentContentName = "Минималното съдържание трябва да е поне 1 символ";
+    }
+}
--- src/RecipesApp.Core/Models/CommentInputModel.cs
+++ src/RecipesApp.Core/Models/CommentInputModel.cs
@@ -1,11 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using RecipesApp.Core.Constants;
+using System.ComponentModel.DataAnnotations;
 
 namespace RecipesApp.Core.Models
 {
     public class CommentInputModel
     {
-        [Required]
-        [MinLength(1)]
+        [Required(ErrorMessage = CommentErrorMessages.CommentContentRequired)]
+        [MinLength(1, ErrorMessage = CommentErrorMessages.CommentContentName)]
         public string Content { get; set; }
 
         public int RecipeId { get; set; }
