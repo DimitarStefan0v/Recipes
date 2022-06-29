68830b89f96d6e101d5f6dc8b848271c35142367
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-06-29 18:26:17 +0300

    Add CommentInputModel

---
+++ src/RecipesApp.Core/Models/CommentInputModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipesApp.Core.Models
+{
+    public class CommentInputModel
+    {
+        [Required]
+        [MinLength(1)]
+        public string Content { get; set; }
+
+        public int RecipeId { get; set; }
+
+    }
+}
