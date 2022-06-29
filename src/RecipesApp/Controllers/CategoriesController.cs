f5ebd1ca351002370398643b2ada48646f94f676
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-06-29 23:47:08 +0300

    Add more html to ById view

--- src/RecipesApp/Controllers/CategoriesController.cs
+++ src/RecipesApp/Controllers/CategoriesController.cs
@@ -38,8 +38,9 @@ namespace RecipesApp.Controllers
             };
 
             viewModel.RecipesCount = viewModel.Recipes.Count();
-            var category = categoriesService.GetAllCategories().Where(x => x.Id == id).FirstOrDefault();
+            var category = categoriesService.GetCategoriesWithImages().Where(x => x.Id == id).FirstOrDefault();
             TempData["CategoryName"] = category.Name;
+            TempData["CategoryImage"] = category.ImgUrl;
 
             return View(viewModel);
         }
