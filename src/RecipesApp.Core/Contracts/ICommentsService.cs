71a7709f170e26743807bbc433bdf4ab384a9589
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-06-29 18:29:35 +0300

    Add interface ICommentsService

---
+++ src/RecipesApp.Core/Contracts/ICommentsService.cs
@@ -0,0 +1,9 @@
+﻿using RecipesApp.Core.Models;
+
+namespace RecipesApp.Core.Contracts
+{
+    public interface ICommentsService
+    {
+        Task AddAsync(CommentInputModel input, string userId);
+    }
+}
