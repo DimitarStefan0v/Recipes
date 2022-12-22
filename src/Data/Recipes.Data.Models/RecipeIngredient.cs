93b76036bbdd3ffb4f5fbfa465cd49191ca07602
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-12-22 20:11:01 +0200

    Add RecipeIngredient entity

---
+++ src/Data/Recipes.Data.Models/RecipeIngredient.cs
@@ -0,0 +1,17 @@
+﻿namespace Recipes.Data.Models
+{
+    using Recipes.Data.Common.Models;
+
+    public class RecipeIngredient : BaseDeletableModel<int>
+    {
+        public int RecipeId { get; set; }
+
+        public Recipe Recipe { get; set; }
+
+        public int IngredientId { get; set; }
+
+        public Ingredient Ingredient { get; set; }
+
+        public string Quantity { get; set; }
+    }
+}
