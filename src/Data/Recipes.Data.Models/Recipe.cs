2bce3e6e7f27165b1f3360cee436acda1f4e64a9
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-12-22 20:10:24 +0200

    Add Recipe entity

---
+++ src/Data/Recipes.Data.Models/Recipe.cs
@@ -0,0 +1,34 @@
+﻿namespace Recipes.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Recipes.Data.Common.Models;
+
+    public class Recipe : BaseDeletableModel<int>
+    {
+        public Recipe()
+        {
+            this.Ingredients = new HashSet<RecipeIngredient>();
+        }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public TimeSpan? PreparationTime { get; set; }
+
+        public TimeSpan? CookingTime { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        public ICollection<RecipeIngredient> Ingredients { get; set; }
+    }
+}
