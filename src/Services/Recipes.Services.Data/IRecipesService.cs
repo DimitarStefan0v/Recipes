620846125d1458111ee43c385ff335869df872cd
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-12-22 21:22:22 +0200

    Describe CreateAsync method

---
+++ src/Services/Recipes.Services.Data/IRecipesService.cs
@@ -0,0 +1,14 @@
+﻿namespace Recipes.Services.Data
+{
+    using Recipes.Web.ViewModels.Recipes;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IRecipesService
+    {
+        Task CreateAsync(CreateRecipeInputModel)
+    }
+}
