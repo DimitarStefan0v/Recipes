e340becc51da20ab4d324cd8945434bcf3907f41
Dimitar Stefanov <dimitarnew9@gmail.com>
2022-12-29 22:22:16 +0200

    Add Create view

--- src/Web/Recipes.Web/Controllers/RecipesController.cs
+++ src/Web/Recipes.Web/Controllers/RecipesController.cs
@@ -1,12 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace Recipes.Web.Controllers
+﻿namespace Recipes.Web.Controllers
 {
-    public class RecipesController : Controller
+    using System;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Recipes.Data.Models;
+    using Recipes.Services.Data;
+    using Recipes.Web.ViewModels.Recipes;
+
+    public class RecipesController : BaseController
     {
-        public IActionResult Index()
+        private readonly IRecipesService recipesService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public RecipesController(IRecipesService recipesService, UserManager<ApplicationUser> userManager)
         {
-            return View();
+            this.recipesService = recipesService;
+            this.userManager = userManager;
+        }
+
+        [Authorize]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateRecipeInputModel input)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(input);
+            }
+
+            var user = await this.userManager.GetUserAsync(this.User);
+
+            try
+            {
+                await this.recipesService.CreateAsync(input, user.Id);
+            }
+            catch (Exception ex)
+            {
+                this.ModelState.AddModelError(string.Empty, ex.Message);
+                return this.View(input);
+            }
+
+            return this.Redirect("/");
         }
     }
 }
