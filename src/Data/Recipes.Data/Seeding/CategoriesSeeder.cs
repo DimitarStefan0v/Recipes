1b33314eb04b5935f29b8294ede6856a98f907cd
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-01-02 10:05:34 +0200

    Add CategoriesSeeder

---
+++ src/Data/Recipes.Data/Seeding/CategoriesSeeder.cs
@@ -0,0 +1,30 @@
+﻿namespace Recipes.Data.Seeding
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Recipes.Data.Models;
+
+    public class CategoriesSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.Categories.Any())
+            {
+                return;
+            }
+
+            await dbContext.Categories.AddAsync(new Category { Name = "Основни ястия" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Супи" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Салати" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Предястия" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Десерти" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Тестени" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Сосове" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Вегетариански и веган" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Зимнина" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Други" });
+        }
+    }
+}
