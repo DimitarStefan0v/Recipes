188f1d77f516861433193e4775d1091d95675b66
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-01-02 10:55:06 +0200

    Add RecipeErrorMessages static class

---
+++ src/Web/Recipes.Web.ViewModels/Constants/RecipeErrorMessages.cs
@@ -0,0 +1,13 @@
+﻿namespace Recipes.Web.ViewModels.Constants
+{
+    public static class RecipeErrorMessages
+    {
+        public const string NameRequired = "Името на рецептата е задължително";
+
+        public const string NameLength = "Името на рецептата трябва да е с дължина между 3 и 50 символа";
+
+        public const string InstructionsRequired = "Начина на приготвяне е задължителен";
+
+        public const string InstructionsLength = "Начина на приготвяне трябва да е с дължина минимум 10 символа";
+    }
+}
