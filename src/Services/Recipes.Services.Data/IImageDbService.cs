bc2182e6cffd533b9b64267a20be95385ef4f270
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-01-02 18:28:38 +0200

    Add IImageDbService interface

---
+++ src/Services/Recipes.Services.Data/IImageDbService.cs
@@ -0,0 +1,11 @@
+﻿namespace Recipes.Services.Data
+{
+    using System.Threading.Tasks;
+
+    public interface IImageDbService
+    {
+        Task<int> WriteToDatabasebAsync(string imageUrl, string imagePublicId);
+
+        string GetPublicId(int id);
+    }
+}
