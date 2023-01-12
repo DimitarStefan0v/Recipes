323529faa6016aac23635060c6a902a8dd24aa60
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-01-12 19:25:55 +0200

    Add RecipeInListViewModel

---
+++ src/Web/Recipes.Web.ViewModels/Recipes/RecipeInListViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Recipes.Web.ViewModels.Recipes
+{
+    using AutoMapper;
+    using global::Recipes.Data.Models;
+    using global::Recipes.Services.Mapping;
+
+    public class RecipeInListViewModel : IMapFrom<Recipe>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<Recipe, RecipeInListViewModel>()
+                .ForMember(x => x.ImageUrl, opt => opt.MapFrom(r => r.Image));
+        }
+    }
+}
