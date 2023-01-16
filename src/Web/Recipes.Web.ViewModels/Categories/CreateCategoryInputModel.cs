7879bfb2d35bd3c85aa4d7c17c292c7873a8036b
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-01-16 11:17:04 +0200

    Add CreateCategoryInputModel

---
+++ src/Web/Recipes.Web.ViewModels/Categories/CreateCategoryInputModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Recipes.Web.ViewModels.Categories
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using global::Recipes.Common.Attributes;
+    using global::Recipes.Common.Constants;
+    using Microsoft.AspNetCore.Http;
+
+    public class CreateCategoryInputModel
+    {
+        [Display(Name = "Заглавие на категорията")]
+        [Required(ErrorMessage = CategoryErrorMessages.NameRequired)]
+        [MinLength(4, ErrorMessage = CategoryErrorMessages.NameLength)]
+        [MaxLength(20, ErrorMessage = CategoryErrorMessages.NameLength)]
+        public string Name { get; set; }
+
+        [Display(Name = "Цвят на категорията")]
+        public string Color { get; set; }
+
+        [Display(Name = "Снимка на категорията")]
+        [MaxFileSize(4 * 1024 * 1024)]
+        [AllowedExtensions(new string[] { ".jpg", ".png", ".gif", ".jpeg" })]
+        public IFormFile Image { get; set; }
+    }
+}
