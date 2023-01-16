cb784cd45416e1b5a327eb92fd6ad5f3ea0ea32d
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-01-16 11:11:21 +0200

    Add CategoryErrorMessages static class

---
+++ src/Recipes.Common/Constants/CategoryErrorMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace Recipes.Common.Constants
+{
+    public static class CategoryErrorMessages
+    {
+        public const string NameRequired = "Името на категорията е задължително";
+
+        public const string NameLength = "Името на категорията трябва да е с дължина между 4 и 20 символа";
+    }
+}
