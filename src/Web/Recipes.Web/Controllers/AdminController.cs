dc116d1aec346cb8239218d51246c92c16394c95
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-01-18 21:32:58 +0200

    Add AdminController

---
+++ src/Web/Recipes.Web/Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+﻿namespace Recipes.Web.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Recipes.Common;
+
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    public class AdminController : Controller
+    {
+    }
+}
