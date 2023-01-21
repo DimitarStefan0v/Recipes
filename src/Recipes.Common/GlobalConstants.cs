285147dd3ff2bcc88f08e102c7509f1fffbbf902
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-01-21 23:03:15 +0200

    Add default category color as constant

--- src/Recipes.Common/GlobalConstants.cs
+++ src/Recipes.Common/GlobalConstants.cs
@@ -5,5 +5,7 @@
         public const string SystemName = "Recipes";
 
         public const string AdministratorRoleName = "Administrator";
+
+        public const string CategoryDefaultColor = "#FFA200";
     }
 }
