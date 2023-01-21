9ed0f3aabe72df396e69771f6d6628e2432ecabc
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-01-21 18:12:14 +0200

    Move color prop to SingleRecipeViewModel and add custom mapping

--- src/Web/Recipes.Web.ViewModels/Recipes/SingleRecipeViewModel.cs
+++ src/Web/Recipes.Web.ViewModels/Recipes/SingleRecipeViewModel.cs
@@ -23,6 +23,8 @@
 
         public int PortionsCount { get; set; }
 
+        public string Color { get; set; }
+
         public string ImageUrl { get; set; }
 
         public IEnumerable<IngredientViewModel> Ingredients { get; set; }
@@ -36,7 +38,8 @@
             configuration.CreateMap<Recipe, SingleRecipeViewModel>()
                 .ForMember(x => x.ImageUrl, opt => opt.MapFrom(r => r.Image.PictureUrl))
                 .ForMember(x => x.PreparationTime, opt => opt.MapFrom(r => (int)r.PreparationTime.Value.TotalMinutes))
-                .ForMember(x => x.CookingTime, opt => opt.MapFrom(r => (int)r.CookingTime.Value.TotalMinutes));
+                .ForMember(x => x.CookingTime, opt => opt.MapFrom(r => (int)r.CookingTime.Value.TotalMinutes))
+                .ForMember(x => x.Color, opt => opt.MapFrom(r => r.Category.Color == null ? "FFA200" : r.Category.Color));
         }
     }
 }
