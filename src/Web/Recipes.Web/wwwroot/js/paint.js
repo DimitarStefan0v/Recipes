4ddd6dfb58f9f929c3dbea044d022a294b39418b
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-01-21 18:22:56 +0200

    Add paint.js for painting wrappen in recipe byId depending on category

--- src/Web/Recipes.Web/wwwroot/js/paint.js
+++ src/Web/Recipes.Web/wwwroot/js/paint.js
@@ -1,5 +1,7 @@
-﻿const ColorCategory = function () {
-   
+﻿const PaintRecipeByIdWrapper = function () {
+    const color = document.getElementById('recipe-container-color');
+    const wrapper = color.parentElement;
+    wrapper.style.backgroundColor = '#' + color.value;
 }
 
-window.onload = ColorCategory();
+window.onload = PaintRecipeByIdWrapper();
