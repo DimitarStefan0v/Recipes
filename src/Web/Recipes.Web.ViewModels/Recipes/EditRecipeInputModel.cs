94b4a8dbf2db0e4a6c43a2a0bce0d99eab563e2c
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-01-21 11:07:18 +0200

    Revert "Revert "Add styles for edit and delete btns at big and small screens""
    This reverts commit 592d6356a39edc55cbc55fc445f8347294527015.

---
+++ src/Web/Recipes.Web.ViewModels/Recipes/EditRecipeInputModel.cs
@@ -0,0 +1,39 @@
+﻿namespace Recipes.Web.ViewModels.Recipes
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using AutoMapper;
+    using global::Recipes.Common.Constants;
+    using global::Recipes.Data.Models;
+    using global::Recipes.Services.Mapping;
+
+    public class EditRecipeInputModel : IMapFrom<Recipe>, IHaveCustomMappings
+    {
+        [Display(Name = "Заглавие на рецептата")]
+        [Required(ErrorMessage = RecipeErrorMessages.NameRequired)]
+        [MinLength(3, ErrorMessage = RecipeErrorMessages.NameLength)]
+        [MaxLength(50, ErrorMessage = RecipeErrorMessages.NameLength)]
+        public string Name { get; set; }
+
+        [Display(Name = "Начин на приготвяне")]
+        [Required(ErrorMessage = RecipeErrorMessages.DescriptionRequired)]
+        [MinLength(10, ErrorMessage = RecipeErrorMessages.DescriptionLength)]
+        public string Description { get; set; }
+
+        [Display(Name = "Време за приготвяне /минути/")]
+        public int? PreparationTime { get; set; }
+
+        [Display(Name = "Време за готвене /минути/")]
+        public int? CookingTime { get; set; }
+
+        [Display(Name = "Порции")]
+        public int? PortionsCount { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<Recipe, EditRecipeInputModel>()
+                .ForMember(x => x.PreparationTime, opt => opt.MapFrom(r => (int)r.PreparationTime.Value.TotalMinutes))
+                .ForMember(x => x.CookingTime, opt => opt.MapFrom(r => (int)r.CookingTime.Value.TotalMinutes));
+        }
+    }
+}
