a1a8cc440786fadb1db0600d6c04e17c259c1eb5
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-01-22 16:18:01 +0200

    Make RecipesListViewModel inherit PagingViewModel so we can add pagination for All page

--- src/Web/Recipes.Web.ViewModels/Recipes/RecipesListViewModel.cs
+++ src/Web/Recipes.Web.ViewModels/Recipes/RecipesListViewModel.cs
@@ -2,7 +2,7 @@
 {
     using System.Collections.Generic;
 
-    public class RecipesListViewModel
+    public class RecipesListViewModel : PagingViewModel
     {
         public IEnumerable<RecipeInListViewModel> Recipes { get; set; }
     }
