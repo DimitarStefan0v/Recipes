9a3884740e2803e18ff340065703b9205c7f2530
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-01-23 12:10:26 +0200

    Change email field with username at login view and change it at the input model

--- src/Web/Recipes.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/Web/Recipes.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,6 +71,10 @@ namespace Recipes.Web.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            [Display(Name = "Потребителско име")]
+            [DataType(DataType.Text)]
+            public string UserName { get; set; }
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -98,10 +102,6 @@ namespace Recipes.Web.Areas.Identity.Pages.Account
             [Display(Name = "Потвърди паролата")]
             [Compare("Password", ErrorMessage = "Паролата и паролата за потвърждение не съвпадат.")]
             public string ConfirmPassword { get; set; }
-
-            [Required]
-            [Display(Name = "Потребителско име")]
-            public string UserName { get; set; }
         }
 
 
