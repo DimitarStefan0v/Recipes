850bd42e5bfbe83052f7d7645cf42371f1067ebf
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-01-23 15:25:18 +0200

    Add api VotesController

---
+++ src/Web/Recipes.Web/Controllers/VotesController.cs
@@ -0,0 +1,41 @@
+﻿namespace Recipes.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Recipes.Data.Models;
+    using Recipes.Services.Data;
+    using Recipes.Web.ViewModels.Votes;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VotesController : Controller
+    {
+        private readonly IVotesService votesService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public VotesController(IVotesService votesService, UserManager<ApplicationUser> userManager)
+        {
+            this.votesService = votesService;
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<ActionResult<PostVoteResponseViewModel>> Post(PostVoteInputModel input)
+        {
+            var user = await this.userManager.GetUserAsync(this.User);
+            await this.votesService.SetVoteAsync(input.RecipeId, user.Id, input.Value);
+
+            var averageVotes = this.votesService.GetAverageVotes(input.RecipeId);
+            var postVoteResponse = new PostVoteResponseViewModel
+            {
+                AverageVotes = averageVotes,
+            };
+
+            return postVoteResponse;
+        }
+    }
+}
