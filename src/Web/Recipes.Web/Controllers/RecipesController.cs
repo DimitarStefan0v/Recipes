3dea01f1ada9867617b0a26a82adcef13e064796
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-01-24 18:08:31 +0200

    Add AllUnapproved action which only administrators can access

--- src/Web/Recipes.Web/Controllers/RecipesController.cs
+++ src/Web/Recipes.Web/Controllers/RecipesController.cs
@@ -91,6 +91,30 @@
             return this.View(viewModel);
         }
 
+        [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+        public IActionResult AllUnapproved(int id = 1)
+        {
+            if (id <= 0)
+            {
+                return this.NotFound();
+            }
+
+            int itemsPerPage = 1;
+
+            var viewModel = new RecipesListViewModel
+            {
+                ItemsPerPage = itemsPerPage,
+                PageNumber = id,
+                ItemsCount = this.recipesService.GetUnapprovedRecipesCount(),
+                Recipes = this.recipesService.GetAllUnapproved<RecipeInListViewModel>(id, itemsPerPage),
+            };
+
+            viewModel.ControllerName = this.ControllerContext.ActionDescriptor.ControllerName;
+            viewModel.ActionName = this.ControllerContext.ActionDescriptor.ActionName;
+
+            return this.View(viewModel);
+        }
+
         public IActionResult AllByName([FromQuery(Name = "search")]string name, int id = 1)
         {
             if (string.IsNullOrWhiteSpace(name) || id <= 0)
