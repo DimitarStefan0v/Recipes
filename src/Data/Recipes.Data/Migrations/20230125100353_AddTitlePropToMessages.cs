74b3504a3d265f62dbf184b25a4b37440facc301
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-01-25 12:05:42 +0200

    Add ContactInputModel

---
+++ src/Data/Recipes.Data/Migrations/20230125100353_AddTitlePropToMessages.cs
@@ -0,0 +1,25 @@
+﻿#nullable disable
+
+namespace Recipes.Data.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class AddTitlePropToMessages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Messages");
+        }
+    }
+}
---
+++ src/Web/Recipes.Web.ViewModels/Home/ContactInputModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Recipes.Web.ViewModels.Home
+{
+    public class ContactInputModel
+    {
+        public string Names { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string AddedByUserUserName { get; set; }
+    }
+}
