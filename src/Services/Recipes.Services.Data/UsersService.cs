31b8337064d2a84a6a7715652dc893fe41ee8e26
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-01-25 16:46:26 +0200

    Implement AddRecipeToFavoritesAsync method in UsersService

--- src/Services/Recipes.Services.Data/UsersService.cs
+++ src/Services/Recipes.Services.Data/UsersService.cs
@@ -1,5 +1,6 @@
 ﻿namespace Recipes.Services.Data
 {
+    using System.Linq;
     using System.Threading.Tasks;
 
     using Recipes.Data.Common.Repositories;
@@ -9,10 +10,34 @@
     public class UsersService : IUsersService
     {
         private readonly IDeletableEntityRepository<Message> messagesRepository;
+        private readonly IDeletableEntityRepository<FavoriteRecipe> favoriteRecipesRepository;
+        private readonly IDeletableEntityRepository<Recipe> recipesRepository;
 
-        public UsersService(IDeletableEntityRepository<Message> messagesRepository)
+        public UsersService(
+            IDeletableEntityRepository<Message> messagesRepository,
+            IDeletableEntityRepository<FavoriteRecipe> favoriteRecipesRepository,
+            IDeletableEntityRepository<Recipe> recipesRepository)
         {
             this.messagesRepository = messagesRepository;
+            this.favoriteRecipesRepository = favoriteRecipesRepository;
+            this.recipesRepository = recipesRepository;
+        }
+
+        public async Task AddRecipeToFavoritesAsync(int recipeId, string userId)
+        {
+            var recipe = this.recipesRepository.AllAsNoTracking().Where(x => x.Id == recipeId).FirstOrDefault();
+            if (userId == null || recipe == null)
+            {
+                return;
+            }
+
+            var favoriteRecipe = new FavoriteRecipe
+            {
+                RecipeId = recipeId,
+                AddedByUserId = userId,
+            };
+
+            await this.favoriteRecipesRepository.AddAsync(favoriteRecipe);
         }
 
         public async Task CreateMessageAsync(ContactInputModel input, string userId)
