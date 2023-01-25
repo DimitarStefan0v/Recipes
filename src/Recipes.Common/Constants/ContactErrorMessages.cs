7d193b89db70a95705d0b1cf4240d0d5617e1628
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-01-25 14:47:39 +0200

    Add ContactErrorMessages static class

---
+++ src/Recipes.Common/Constants/ContactErrorMessages.cs
@@ -0,0 +1,19 @@
+﻿namespace Recipes.Common.Constants
+{
+    public static class ContactErrorMessages
+    {
+        public const string NameRequired = "Името е задължително";
+
+        public const string NameLength = "Името трябва да е с дължина от поне 3 букви";
+
+        public const string EmailRequired = "Вашият email адрес е задължителен";
+
+        public const string TitleRequired = "Заглавие на съобщението е задължително";
+
+        public const string TitleLength = "Заглавието трябва да е с дължина от поне 5 букви";
+
+        public const string ContentRequired = "Съдържание към съобщението е задължително";
+
+        public const string ContentLength = "Съдържанието трябва да е с дължина от поне 10 букви";
+    }
+}
