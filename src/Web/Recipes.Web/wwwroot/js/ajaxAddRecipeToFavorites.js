45a031c9d474ef2922dae8b1eb9b1fe5f206fed1
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-01-25 20:19:42 +0200

    Add the forgotten antiForgeryToken

--- src/Web/Recipes.Web/wwwroot/js/ajaxAddRecipeToFavorites.js
+++ src/Web/Recipes.Web/wwwroot/js/ajaxAddRecipeToFavorites.js
@@ -1,16 +1,18 @@
 ﻿const AddRecipeToUserFavorites = function () {
     const article = document.querySelector('.favorites');
     const i = article.querySelector('i');
-    i.addEventListener('click' async () => {
+    i.addEventListener('click', async () => {
         const value = document.querySelector('.favorites span');
-        if (value.textContent == 'False') {
+        if (value.textContent == 'false') {
             const recipeId = document.getElementById('recipe-id').value;
+            const antiForgeryToken = document.querySelector('#antiForgeryForm input[name=__RequestVerificationToken]').value;
             let data = { recipeId: recipeId };
             const url = '/api/Favorites';
             const options = {
                 method: 'post',
                 headers: {
                     'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': antiForgeryToken,
                     },
                 body: JSON.stringify(data),
             };
