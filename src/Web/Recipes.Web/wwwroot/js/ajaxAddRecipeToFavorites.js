320f291292586921db71cce357c50f9872d96c79
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-01-25 19:29:40 +0200

    Add AJAX adding recipe to user favorites

---
+++ src/Web/Recipes.Web/wwwroot/js/ajaxAddRecipeToFavorites.js
@@ -0,0 +1,26 @@
+﻿const AddRecipeToUserFavorites = function () {
+    const article = document.querySelector('.favorites');
+    const i = article.querySelector('i');
+    i.addEventListener('click' async () => {
+        const value = document.querySelector('.favorites span');
+        if (value.textContent == 'False') {
+            const recipeId = document.getElementById('recipe-id').value;
+            let data = { recipeId: recipeId };
+            const url = '/api/Favorites';
+            const options = {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                    },
+                body: JSON.stringify(data),
+            };
+
+            const response = await fetch(url, options);
+            const result = await response.json();
+
+            value.textContent = result;
+        }
+    });
+}
+
+window.onload = AddRecipeToUserFavorites();
