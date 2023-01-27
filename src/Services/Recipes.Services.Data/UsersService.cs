9be8c297b6c17c54d7106a98a6ffb2afdc14e9af
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-01-27 19:28:38 +0200

    Implement GetFavorites method

--- src/Services/Recipes.Services.Data/UsersService.cs
+++ src/Services/Recipes.Services.Data/UsersService.cs
@@ -1,26 +1,33 @@
 ﻿namespace Recipes.Services.Data
 {
+    using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
 
+    using Recipes.Common;
     using Recipes.Data.Common.Repositories;
     using Recipes.Data.Models;
+    using Recipes.Services.Mapping;
     using Recipes.Web.ViewModels.Home;
+    using Recipes.Web.ViewModels.Recipes;
 
     public class UsersService : IUsersService
     {
         private readonly IDeletableEntityRepository<Message> messagesRepository;
         private readonly IDeletableEntityRepository<FavoriteRecipe> favoriteRecipesRepository;
         private readonly IDeletableEntityRepository<Recipe> recipesRepository;
+        private readonly IDeletableEntityRepository<ApplicationUser> usersRepository;
 
         public UsersService(
             IDeletableEntityRepository<Message> messagesRepository,
             IDeletableEntityRepository<FavoriteRecipe> favoriteRecipesRepository,
-            IDeletableEntityRepository<Recipe> recipesRepository)
+            IDeletableEntityRepository<Recipe> recipesRepository,
+            IDeletableEntityRepository<ApplicationUser> usersRepository)
         {
             this.messagesRepository = messagesRepository;
             this.favoriteRecipesRepository = favoriteRecipesRepository;
             this.recipesRepository = recipesRepository;
+            this.usersRepository = usersRepository;
         }
 
         public async Task AddRecipeToFavoritesAsync(int recipeId, string userId)
@@ -53,7 +60,7 @@
                 return;
             }
 
-            this.favoriteRecipesRepository.Delete(recipe);
+            this.favoriteRecipesRepository.HardDelete(recipe);
             await this.favoriteRecipesRepository.SaveChangesAsync();
         }
 
@@ -61,11 +68,11 @@
         {
             var message = new Message
             {
-                 Title = input.Title.Trim(),
-                 Content = input.Content.Trim(),
-                 Name = input.Name.Trim(),
-                 Email = input.Email.Trim(),
-                 AddedByUserId = userId,
+                Title = input.Title.Trim(),
+                Content = input.Content.Trim(),
+                Name = input.Name.Trim(),
+                Email = input.Email.Trim(),
+                AddedByUserId = userId,
             };
 
             await this.messagesRepository.AddAsync(message);
@@ -80,6 +87,30 @@
                 .FirstOrDefault();
 
             return favoriteRecipe == null ? false : true;
+        }
+
+        public IEnumerable<T> GetFavorites<T>(int page, int itemsPerPage, string userId)
+        {
+            var favoriteRecipes = this.favoriteRecipesRepository
+                .AllAsNoTracking()
+                .Where(x => x.AddedByUserId == userId)
+                .Select(x => x.RecipeId)
+                .ToList();
+
+            var recipesToReturn = new HashSet<T>();
+
+            foreach (var favoriteRecipeId in favoriteRecipes)
+            {
+                var recipe = this.recipesRepository
+                    .AllAsNoTracking()
+                    .Where(x => x.Id == favoriteRecipeId)
+                    .To<T>()
+                    .FirstOrDefault();
+
+                recipesToReturn.Add(recipe);
+            }
+
+            return recipesToReturn;
         }
     }
 }
