e23fbaca948d766a1bd57e5e251f6ad1fc101cc8
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-02-09 12:36:35 +0200

    Add MessageInListViewModel

---
+++ src/Web/Recipes.Web.ViewModels/Messages/MessageInListViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Recipes.Web.ViewModels.Messages
+{
+    using global::Recipes.Data.Models;
+    using global::Recipes.Services.Mapping;
+
+    public class MessageInListViewModel : IMapFrom<Message>
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string AddedByUserUserName { get; set; }
+
+        public string Name { get; set; }
+
+        public string Ip { get; set; }
+
+        public string Email { get; set; }
+    }
+}
