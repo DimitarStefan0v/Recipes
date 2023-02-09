76aba28f4d8916a1c8c7aaa84d23eeee74161c32
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-02-09 12:04:40 +0200

    Add IMessagesService interface

---
+++ src/Services/Recipes.Services.Data/IMessagesService.cs
@@ -0,0 +1,15 @@
+﻿namespace Recipes.Services.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Recipes.Web.ViewModels.Home;
+
+    public interface IMessagesService
+    {
+        Task CreateMessageAsync(ContactInputModel input, string userId);
+    }
+}
