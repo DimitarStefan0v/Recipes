a787951e43c9ea670e697b412f62083daf0d2b3b
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-02-09 13:34:59 +0200

    Add anchor with link to delete action and tag for time of created message

--- src/Web/Recipes.Web.ViewModels/Messages/MessageInListViewModel.cs
+++ src/Web/Recipes.Web.ViewModels/Messages/MessageInListViewModel.cs
@@ -1,10 +1,15 @@
 ﻿namespace Recipes.Web.ViewModels.Messages
 {
+    using System;
+    using System.Globalization;
+
     using global::Recipes.Data.Models;
     using global::Recipes.Services.Mapping;
 
     public class MessageInListViewModel : IMapFrom<Message>
     {
+        public int Id { get; set; }
+
         public string Title { get; set; }
 
         public string Content { get; set; }
@@ -14,5 +19,9 @@
         public string Name { get; set; }
 
         public string Email { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string DateAsString => this.CreatedOn.ToString("ddd, MM dd, yyyy HH:mm", new CultureInfo("bg-Bg"));
     }
 }
