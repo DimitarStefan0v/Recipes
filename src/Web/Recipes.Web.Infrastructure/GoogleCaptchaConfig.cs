f2f08afcca1d1c5bf72745af96780a8c64ea977e
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-02-10 16:33:40 +0200

    Add GoogleCaptchaConfig in Infrastructure

---
+++ src/Web/Recipes.Web.Infrastructure/GoogleCaptchaConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Recipes.Web.Infrastructure
+{
+    public class GoogleCaptchaConfig
+    {
+        public string SiteKey { get; set; }
+
+        public string SecretKey { get; set; }
+    }
+}
