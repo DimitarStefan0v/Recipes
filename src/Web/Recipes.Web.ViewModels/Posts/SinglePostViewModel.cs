8b5cf99ed797e4b3984092bfc9e01d9231e32eb5
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-02-12 19:37:36 +0200

    Add SinglePostViewModel

---
+++ src/Web/Recipes.Web.ViewModels/Posts/SinglePostViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Recipes.Web.ViewModels.Posts
+{
+    using System;
+    using System.Globalization;
+
+    using global::Recipes.Data.Models;
+    using global::Recipes.Services.Mapping;
+
+    public class SinglePostViewModel : IMapFrom<Post>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Content { get; set; }
+
+        public string AddedByUserUserName { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string DateAsString => this.CreatedOn.ToString("ddd, MM dd, yyyy HH:mm", new CultureInfo("bg-Bg"));
+    }
+}
