92a884c9c52a2cf6eebb94f5b4f365e17b0ac2a2
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-02-12 15:21:11 +0200

    Add PostsListViewModel

---
+++ src/Web/Recipes.Web.ViewModels/Posts/PostsListViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Recipes.Web.ViewModels.Posts
+{
+    using System.Collections.Generic;
+
+    public class PostsListViewModel
+    {
+        public IEnumerable<PostInListViewModel> Posts { get; set; }
+    }
+}
