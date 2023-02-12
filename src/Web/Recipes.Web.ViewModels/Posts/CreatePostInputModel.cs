62fda5d55e4a121924cacc0bb32caf74c07608d7
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-02-12 15:56:08 +0200

    Add CreatePostInputModel

---
+++ src/Web/Recipes.Web.ViewModels/Posts/CreatePostInputModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Recipes.Web.ViewModels.Posts
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using global::Recipes.Common.Constants;
+
+    public class CreatePostInputModel
+    {
+        [Display(Name = "Заглавие на поста")]
+        [Required(ErrorMessage = PostErrorMessages.NameRequired)]
+        [MinLength(3, ErrorMessage = PostErrorMessages.NameLength)]
+        [MaxLength(50, ErrorMessage = PostErrorMessages.NameLength)]
+        public string Name { get; set; }
+
+        [Display(Name = "Съдържание към поста")]
+        [Required(ErrorMessage = PostErrorMessages.ContentRequired)]
+        [MinLength(10, ErrorMessage = PostErrorMessages.ContentLength)]
+        public string Content { get; set; }
+    }
+}
