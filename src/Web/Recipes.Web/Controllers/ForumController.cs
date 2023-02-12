0abe865b4f31930bbdc3c94226caada0d8805757
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-02-12 16:15:53 +0200

    Add ForumController and Index action to display all posts

---
+++ src/Web/Recipes.Web/Controllers/ForumController.cs
@@ -0,0 +1,41 @@
+﻿namespace Recipes.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Recipes.Services.Data.Contracts;
+    using Recipes.Web.ViewModels.Posts;
+
+    public class ForumController : BaseController
+    {
+        private readonly IPostsService postsService;
+        private readonly ICountsService countsService;
+
+        public ForumController(IPostsService postsService, ICountsService countsService)
+        {
+            this.postsService = postsService;
+            this.countsService = countsService;
+        }
+
+        public IActionResult Index(string sortOrder = "descending", int id = 1)
+        {
+            if (id <= 0)
+            {
+                return this.NotFound();
+            }
+
+            int itemsPerPage = 9;
+
+            var viewModel = new PostsListViewModel
+            {
+                ItemsPerPage = itemsPerPage,
+                PageNumber = id,
+                ItemsCount = this.countsService.GetPostsCount(),
+                Posts = this.postsService.GetAll<PostInListViewModel>(sortOrder, id, itemsPerPage),
+                ControllerName = this.ControllerContext.ActionDescriptor.ControllerName,
+                ActionName = this.ControllerContext.ActionDescriptor.ActionName,
+                SortOrder = sortOrder,
+            };
+
+            return this.View(viewModel);
+        }
+    }
+}
