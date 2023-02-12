f3cedbc76b4f355f2301c660e35fd2091ff627b5
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-02-12 19:07:03 +0200

    Add props UserNameOfLastComment, LastCommentDate to PostInListViewModel with custom mapping

--- src/Web/Recipes.Web.ViewModels/Posts/PostInListViewModel.cs
+++ src/Web/Recipes.Web.ViewModels/Posts/PostInListViewModel.cs
@@ -2,11 +2,13 @@
 {
     using System;
     using System.Globalization;
+    using System.Linq;
 
+    using AutoMapper;
     using global::Recipes.Data.Models;
     using global::Recipes.Services.Mapping;
 
-    public class PostInListViewModel : IMapFrom<Post>
+    public class PostInListViewModel : IMapFrom<Post>, IHaveCustomMappings
     {
         public int Id { get; set; }
 
@@ -21,5 +23,24 @@
         public DateTime CreatedOn { get; set; }
 
         public string DateAsString => this.CreatedOn.ToString("ddd, MM dd, yyyy HH:mm", new CultureInfo("bg-Bg"));
+
+        public string UserNameOfLastComment { get; set; }
+
+        public DateTime LastCommentDate { get; set; }
+
+        public string LastCommentDateAsString => this.LastCommentDate.ToString("ddd, MM dd, yyyy HH:mm", new CultureInfo("bg-Bg"));
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<Post, PostInListViewModel>()
+                .ForMember(x => x.UserNameOfLastComment, opt => opt.MapFrom(p => p.Comments
+                                                                .OrderByDescending(c => c.CreatedOn)
+                                                                .Select(c => c.AddedByUser.UserName)
+                                                                .FirstOrDefault()))
+                .ForMember(x => x.LastCommentDate, opt => opt.MapFrom(p => p.Comments
+                                                                .OrderByDescending(c => c.CreatedOn)
+                                                                .Select(c => c.CreatedOn)
+                                                                .FirstOrDefault()));
+        }
     }
 }
