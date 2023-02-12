00768b310acdfbab7e4dd2dad2ef025895de9d92
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-02-12 09:52:01 +0200

    Add Unite test for VotesService test when user votes more than once that only last vote is counted

---
+++ src/Tests/Recipes.Services.Data.Tests/VotesServiceTests.cs
@@ -0,0 +1,36 @@
+﻿namespace Recipes.Services.Data.Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Moq;
+    using Recipes.Data.Common.Repositories;
+    using Recipes.Data.Models;
+    using Xunit;
+
+    public class VotesServiceTests
+    {
+        [Fact]
+        public async Task WhenUserVotesMoreThanOnceOnlyLastVoteShouldCount()
+        {
+            // Arrange
+            var list = new List<Vote>();
+            var mockRepo = new Mock<IDeletableEntityRepository<Vote>>();
+            mockRepo.Setup(x => x.All()).Returns(list.AsQueryable());
+            mockRepo.Setup(x => x.AddAsync(It.IsAny<Vote>())).Callback((Vote vote) => list.Add(vote));
+            var service = new VotesService(mockRepo.Object);
+
+            // Act
+            await service.SetVoteAsync(1, "someUserId", 2);
+            await service.SetVoteAsync(1, "someUserId", 1);
+            await service.SetVoteAsync(1, "someUserId", 5);
+            await service.SetVoteAsync(1, "someUserId", 4);
+
+            // Assert
+            Assert.Equal(4, list.First().Value);
+        }
+
+        
+    }
+}
