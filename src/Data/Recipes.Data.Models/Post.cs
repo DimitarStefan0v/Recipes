7a253b549e9d165a84d9065df66053c139afa6f4
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-02-12 15:03:13 +0200

    Add Post Db Model

---
+++ src/Data/Recipes.Data.Models/Post.cs
@@ -0,0 +1,26 @@
+﻿namespace Recipes.Data.Models
+{
+    using System.Collections.Generic;
+
+    using Recipes.Data.Common.Models;
+
+    public class Post : BaseDeletableModel<int>
+    {
+        public Post()
+        {
+            this.Comments = new HashSet<Comment>();
+        }
+
+        public string Name { get; set; }
+
+        public string Content { get; set; }
+
+        public int ViewsCount { get; set; }
+
+        public string AddedByUserId { get; set; }
+
+        public ApplicationUser AddedByUser { get; set; }
+
+        public ICollection<Comment> Comments { get; set; }
+    }
+}
