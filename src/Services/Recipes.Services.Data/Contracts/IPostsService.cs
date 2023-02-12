9d58451615024d957002f7bc0baa4c5602de1087
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-02-12 15:14:05 +0200

    Add IPostsService interface

---
+++ src/Services/Recipes.Services.Data/Contracts/IPostsService.cs
@@ -0,0 +1,9 @@
+﻿namespace Recipes.Services.Data.Contracts
+{
+    using System.Collections.Generic;
+
+    public interface IPostsService
+    {
+        IEnumerable<T> GetAll<T>(string sort, int page, int itemsPerPage);
+    }
+}
