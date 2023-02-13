89e12fa4b0be77282c06b8c2fe3946faf0b80e3c
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-02-13 08:57:28 +0200

    Add CommentInListViewModel

--- src/Web/Recipes.Web.ViewModels/Posts/SinglePostViewModel.cs
+++ src/Web/Recipes.Web.ViewModels/Posts/SinglePostViewModel.cs
@@ -1,12 +1,14 @@
 ﻿namespace Recipes.Web.ViewModels.Posts
 {
     using System;
+    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.Globalization;
 
     using global::Recipes.Common.Constants;
     using global::Recipes.Data.Models;
     using global::Recipes.Services.Mapping;
+    using global::Recipes.Web.ViewModels.Comments;
 
     public class SinglePostViewModel : IMapFrom<Post>
     {
@@ -21,6 +23,8 @@
         public DateTime CreatedOn { get; set; }
 
         public string DateAsString => this.CreatedOn.ToString("ddd, MM dd, yyyy HH:mm", new CultureInfo("bg-Bg"));
+
+        public IEnumerable<CommentInListViewModel> Comments { get; set; }
 
         [Required(ErrorMessage = CommentErrorMessages.ContentRequired)]
         [MinLength(10, ErrorMessage = CommentErrorMessages.ContentLength)]
